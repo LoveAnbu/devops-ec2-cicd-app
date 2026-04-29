b35b1fe6aa15b525f3e142bb0c55ed88c3a83fc8
Ubuntu <ubuntu@ip-10-19-0-42.ec2.internal>
2026-04-29 13:58:33 +0000

    add prometheus metrics

--- app.js
+++ app.js
@@ -1,4 +1,21 @@
 const http = require('http');
-http.createServer((req, res) => {
-  res.end("Version 3 - Webhook Working 🚀");
-}).listen(3000);
+const client = require('prom-client');
+
+client.collectDefaultMetrics();
+
+const requestCount = new client.Counter({
+  name: 'app_requests_total',
+  help: 'Total number of requests'
+});
+
+const server = http.createServer(async (req, res) => {
+  if (req.url === '/metrics') {
+    res.setHeader('Content-Type', client.register.contentType);
+    res.end(await client.register.metrics());
+  } else {
+    requestCount.inc();
+    res.end("Hello from DevOps App 🚀");
+  }
+});
+
+server.listen(3000);
